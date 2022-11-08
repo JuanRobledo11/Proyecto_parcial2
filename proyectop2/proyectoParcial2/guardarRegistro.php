88a9730d862502f10ffc8902f1928241940df47c
JuanRobledo11 <100009365+JuanRobledo11@users.noreply.github.com>
2022-11-07 19:52:51 -0700

    Update guardarRegistro.php

--- proyectop2/proyectoParcial2/guardarRegistro.php
+++ proyectop2/proyectoParcial2/guardarRegistro.php
@@ -1,17 +1,34 @@
-<?php 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jugador guardado con exito</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+</head>
+<body>
+    
+</body>
+</html>
+
+<?php
+    include 'menu.php';
     include 'conexion.php';
 
     $id = $_POST["id"];
     $nombre = $_POST["nombre"];
-    $empresa_desarrolladora = $_POST["empresa_desarrolladora"];
-    $descripcion = $_POST["descripcion"];
-    $precio = $_POST["precio"];
-    $fecha_salida = $_POST["fecha_salida"];
+    $fecha_nacimiento = $_POST["fecha_nacimiento"];
+    $posicion = $_POST["posicion"];
+    $pais = $_POST["pais"];
+    $peso = $_POST["peso"];
+    $estatura = $_POST["estatura"];
 
-    $sql = "UPDATE juegos SET nombre='".$nombre."', empresa_desarrolladora = '".$empresa_desarrolladora."', descripcion='".$descripcion."', precio='".$precio."', fecha_salida='".$fecha_salida."'"."WHERE id=".$id;
+    $sql = "UPDATE jugador SET nombre='".$nombre."', fecha_nacimiento = '".$fecha_nacimiento."', posicion = '".$posicion."', pais = '".$pais."', peso = '".$peso."', estatura = '".$estatura."'"."WHERE id=".$id;
 
     if($conexion->query($sql) === TRUE){
-        echo "Registro guardado con Éxito <a href='consultarDatos.php'>Regresar</a>";
+        echo "<h1 style='text-align:center; font-size: 35px; margin-top: 30px;'>Registro guardado con éxito <a href='consultarDatos.php'>Regresar</a></h1>";
+        echo "<center><img src='img/like.png'><center>";
     } else {
         echo "Error: ".$sql."<br>".$conexion->error."<br><br><a href='consultarDatos.php'>Regresar</a>";
 
