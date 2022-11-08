a7ad1e4df84b2244b74d9a6df36e54933d44fb55
JuanRobledo11 <100009365+JuanRobledo11@users.noreply.github.com>
2022-11-07 19:38:22 -0700

    Update registrarDatos.php

--- proyectop2/proyectoParcial2/registrarDatos.php
+++ proyectop2/proyectoParcial2/registrarDatos.php
@@ -4,28 +4,29 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Videojuegos</title>
+    <title>Añadir jugador</title>
     <link rel="stylesheet" href="css/bootstrap.css">
     <script src="js/jquery-3.6.1.js"></script>
     <style>
-        textarea{
-            resize: none;
-        }
-
-        h1{
-            text-align: center;
-        }
-
         .form{
             background-color: #fff;
             margin: auto;
             width: 90%;
-            max-width: 700px;
+            max-width: 500px;
             padding: 4.5em 3em;
             border-radius: 10px;
-            box-shadow: 0 5px 10px -5px rgb(0 0 0 / 30%)
+            box-shadow: 0 5px 10px -5px rgb(0 0 0 / 30%);
         }
 
+        h1{
+            text-align: center;
+            color: black;
+            font-size: 50px;
+        }
+
+        body{
+            background-image: url("img/estadio.jpg");
+        }
     </style>
 </head>
 <body>
@@ -34,44 +35,50 @@
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <h1>Registrar Videojuego</h1><hr>
+                <h1>Añadir jugador</h1>
+                <form method="post" action="guardarDatos.php" class="form" style="background: rgba(248,80,50,1);">
 
-                <form method="post" action="guardarDatos.php" class="form">
                     <div class="form-group">
                         <label for="">Nombre:</label>
-                        <input type="text" class="form-control" name="nombre" placeholder="Teclea el nombre">
+                        <input type="text" class="form-control" name="nombre">
                     </div>
 
                     <div class="form-group">
-                        <label for="">Empresa Desarrolladora:</label>
-                        <input type="text" class="form-control" name="empresa_desarrolladora">
+                        <label for="">Fecha de nacimiento:</label>
+                        <input type="date" class="form-control" name="fecha_nacimiento">
                     </div>
 
                     <div class="form-group">
-                        <label for="">Descripción:</label>
-                        <textarea name="descripcion" cols="30" rows="5" class="form-control" placeholder="Ingresar una pequeña reseña"></textarea>
+                        <label for="">Posicion:</label>
+                        <input type="text" class="form-control" name="posicion">
                     </div>
 
                     <div class="form-group">
-                        <label for="">Precio:</label>
-                        <input type="number" class="form-control" name="precio" placeholder="Teclea el precio">
+                        <label for="">Pais:</label>
+                        <input type="text" class="form-control" name="pais">
                     </div>
 
                     <div class="form-group">
-                        <label for="">Fecha de salida:</label>
-                        <input type="date" class="form-control" name="fecha_salida">
+                        <label for="">Peso:</label>
+                        <input type="text" class="form-control" name="peso">
                     </div>
+
+                    <div class="form-group">
+                        <label for="">Estatura:</label>
+                        <input type="text" class="form-control" name="estatura">
+                    </div>
+
                     <center>
                     <div>
                         <input type="submit" class="btn btn-primary" value="Registrar" class="enviar">
                         <a href="consultarDatos.php" class="btn btn-danger">Cancelar</a>
                     </div>
-                    <center>
+                    <center>  
+
                 </form>
             </div>
         </div>
     </div>
-
     <script src="js/bootstrap.js"></script>
 </body>
 </html>
