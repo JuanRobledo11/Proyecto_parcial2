7f4f5cb966096079dca233b6139221d14913a44a
JuanRobledo11 <100009365+JuanRobledo11@users.noreply.github.com>
2022-11-07 19:36:32 -0700

    Update consultarDatos.php

--- proyectop2/proyectoParcial2/consultarDatos.php
+++ proyectop2/proyectoParcial2/consultarDatos.php
@@ -4,16 +4,35 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Videojuegos</title>
+    <title>Lista de jugadores</title>
     <link rel="stylesheet" href="css/bootstrap.css">
     <script src="js/jquery-3.6.1.js"></script>
+    <style>
+        h1{
+            text-align: center;
+            margin-bottom: 10px;
+            color: white;
+        }
+
+        body{
+            background-image: url("img/consultarimg.jpg");
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+            background-size: cover;
+        }
+
+        thead, tr, th, td{
+            color: white;
+        }
+    </style>
 </head>
 <body>
 
     <?php
       include 'conexion.php';
-      $sql = "select * from juegos";
+      $sql = "select * from jugador";
       $datos = $conexion->query($sql);
+
     ?>
 
     <?php include 'menu.php'; ?>
@@ -21,18 +40,17 @@
     <div class="container">
         <div class="row">
             <div class="col-12">
-
-            <h1 class="title" style="text-align:center; padding-bottom: 20px">Lista de deseos</h1>
-
+                <h1>Jugadores</h1>
                 <table class="table table-hover">
                     <thead>
                         <tr>
                             <th>ID</th>
                             <th>Nombre</th>
-                            <th>Empresa Desarrolladora</th>
-                            <th>Descripción</th>
-                            <th>Precio</th>
-                            <th>Fecha de lanzamiento</th>
+                            <th>Fecha de nacimiento</th>
+                            <th>Posicion en el campo</th>
+                            <th>Pais</th>
+                            <th>Peso</th>
+                            <th>Estatura</th>
                             <th>Opciones</th>
                         </tr>
                     </thead>
@@ -43,10 +61,11 @@
                         <tr>
                             <td><?php echo $row["id"]; ?></td>
                             <td><?php echo $row["nombre"]; ?></td>
-                            <td><?php echo $row["empresa_desarrolladora"]; ?></td>
-                            <td><?php echo $row["descripcion"]; ?></td>
-                            <td><?php echo $row["precio"]; ?></td>
-                            <td><?php echo $row["fecha_salida"]; ?></td>
+                            <td><?php echo $row["fecha_nacimiento"]; ?></td>
+                            <td><?php echo $row["posicion"]; ?></td>
+                            <td><?php echo $row["pais"]; ?></td>
+                            <td><?php echo $row["peso"]; ?></td>
+                            <td><?php echo $row["estatura"]; ?></td>
                             <td>
                                 <a href="actualizarRegistro.php?id=<?php echo $row["id"]; ?>" class="btn btn-primary">Editar</a>
                                 <a href="eliminarRegistro.php?id=<?php echo $row["id"]; ?>" class="btn btn-danger">Eliminar</a>
@@ -62,7 +81,6 @@
             </div>
         </div>
     </div>
-
     <script src="js/bootstrap.js"></script>
 </body>
 </html>
