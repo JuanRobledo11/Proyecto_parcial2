9a64b9c3be946fedc4d742d0cbef0fd6b14c52a3
JuanRobledo11 <100009365+JuanRobledo11@users.noreply.github.com>
2022-11-07 19:35:06 -0700

    Update actualizarRegistro.php

--- proyectop2/proyectoParcial2/actualizarRegistro.php
+++ proyectop2/proyectoParcial2/actualizarRegistro.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Videojuegos</title>
+    <title>Actualizar jugador</title>
     <link rel="stylesheet" href="css/bootstrap.css">
     <script src="js/jquery-3.6.1.js"></script>
     <style>
@@ -12,19 +12,28 @@
             background-color: #fff;
             margin: auto;
             width: 90%;
-            max-width: 700px;
+            max-width: 500px;
             padding: 4.5em 3em;
             border-radius: 10px;
             box-shadow: 0 5px 10px -5px rgb(0 0 0 / 30%)
         }
-    </style>
+        
+        h1{
+            text-align: center;
+            color: black;
+            font-size: 50px;
+        }
 
+        body{
+            background-image: url("img/estadio.jpg");
+        }
+    </style>
 </head>
 <body>
     <?php
         include 'conexion.php';
         $id = $_GET["id"];
-        $sql = "SELECT * FROM juegos WHERE id=" . $id;
+        $sql = "SELECT * FROM jugador WHERE id=" . $id;
         $resultado = $conexion->query($sql);
         $registro = $resultado->fetch_assoc();
     ?>
@@ -33,45 +42,51 @@
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <h1>Registrar Videojuego</h1><hr>
-                
-                <form method="post" action="guardarRegistro.php" class="form">
-                    
+                <h1>Editar jugador</h1><hr>
+                <form method="post" action="guardarRegistro.php" class="form" style="background: rgba(248,80,50,1);">
+                                        
                 <input name="id" type="hidden" value="<?php echo $registro["id"]; ?>">
                     <div class="form-group">
                         <label for="">Nombre:</label>
-                        <input value="<?php echo $registro["nombre"]; ?>" type="text" class="form-control" name="nombre" placeholder="Teclea el titulo">
+                        <input value="<?php echo $registro["nombre"]; ?>" type="text" class="form-control" name="nombre">
                     </div>
 
                 <div class="form-group">
-                    <label for="">Empresa Desarrolladora:</label>
-                    <input value="<?php echo $registro["empresa_desarrolladora"]; ?>" type="text" class="form-control" name="empresa_desarrolladora">
-                </div>
-                    
-                <div class="form-group">
-                    <label for="">Descripción:</label>
-                    <textarea value="<?php echo $registro["descripcion"]; ?>" name="descripcion" cols="30" rows="5" class="form-control" placeholder="Ingresar una pequeña reseña"></textarea>
+                    <label for="">Fecha de nacimiento:</label>
+                    <input value="<?php echo $registro["fecha_nacimiento"]; ?>" type="date" class="form-control" name="fecha_nacimiento">
                 </div>
 
                 <div class="form-group">
-                    <label for="">Precio:</label>
-                    <input value="<?php echo $registro["precio"]; ?>" type="number" class="form-control" name="precio">
+                    <label for="">Posicion:</label>
+                    <input value="<?php echo $registro["posicion"]; ?>" type="text" class="form-control" name="posicion">
                 </div>
 
-                 <div class="form-group">
-                    <label for="">Fecha de salida:</label>
-                    <input value="<?php echo $registro["fecha_salida"]; ?>" type="date" class="form-control" name="fecha_salida">
+                <div class="form-group">
+                    <label for="">Pais:</label>
+                    <input value="<?php echo $registro["pais"]; ?>" type="text" class="form-control" name="pais">
                 </div>
-                    <div>
-                        <input type="submit" class="btn btn-primary" value="Registrar">
-                        <a href="consultarDatos.php" class="btn btn-danger">Cancelar</a>
-                    </div>
+
+                <div class="form-group">
+                    <label for="">Peso:</label>
+                    <input value="<?php echo $registro["peso"]; ?>" type="text" class="form-control" name="peso">
+                </div>
+
+                <div class="form-group">
+                    <label for="">Estatura:</label>
+                    <input value="<?php echo $registro["estatura"]; ?>" type="text" class="form-control" name="estatura">
+                </div>
+                
+                <center>
+                <div>
+                    <input type="submit" class="btn btn-primary" value="Registrar">
+                    <a href="consultarDatos.php" class="btn btn-danger">Cancelar</a>
+                </div>
+                </center>
+
                 </form>
             </div>
         </div>
     </div>
-
     <script src="js/bootstrap.js"></script>
 </body>
 </html>
-
